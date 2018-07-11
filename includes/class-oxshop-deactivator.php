ba5d1fe12ae671817f8479cdfafb64832cdd436b
Zorca <vs@zorca.org>
2018-07-11 08:36:05 +0300

    Init

---
+++ includes/class-oxshop-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://zorca.org
+ * @since      1.0.0
+ *
+ * @package    Oxshop
+ * @subpackage Oxshop/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Oxshop
+ * @subpackage Oxshop/includes
+ * @author     Zorca <vs@zorca.org>
+ */
+class Oxshop_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
